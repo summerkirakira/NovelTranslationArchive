fd572fd73b5605262ca10972066503eed25ae48d
Summerkirakira <summerkirakira@gmail.com>
2023-08-24 15:18:10 +0800

    修改持续集成

--- releases.py
+++ releases.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     archive = Archive()
     release_info = archive.validate()
     archive.release(release_original=False)
-    release_text = '# 书籍发布信息\n\n'
+    release_text = '## 书籍发布信息\n\n'
     for index, novel in enumerate(release_info.novels):
-        release_text += f'## {index + 1}. {novel.chinese_title}({novel.title}) 翻译进度{novel.translated_chapter}/{novel.total_chapter}\n\n'
-    release_text += f'## **Translated by Summerkirakira**'
+        release_text += f'### {index + 1}. {novel.chinese_title}({novel.title}) 翻译进度{novel.translated_chapter}/{novel.total_chapter}\n\n'
+    release_text += f'### **Translated by Summerkirakira**'
     sys.stdout = sys.__stdout__
     print(release_text)
