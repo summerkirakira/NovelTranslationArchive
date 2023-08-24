e2c32c8f10266a13b34be5ff7ae71006267a3e83
Summerkirakira <summerkirakira@gmail.com>
2023-08-24 15:02:16 +0800

    修改持续集成

--- releases.py
+++ releases.py
@@ -1,17 +1,18 @@
 from main import Archive
-import time
-import os
 import sys
+import os
+from pathlib import Path
 
 if __name__ == '__main__':
     sys.stdout = open(os.devnull, 'w')
     archive = Archive()
     release_info = archive.validate()
     archive.release(release_original=False)
-    release_text = 'RELEASE_TEXT=# 书籍发布信息\n\n'
+    release_text = '# 书籍发布信息\n\n'
     for index, novel in enumerate(release_info.novels):
         release_text += f'## {index + 1}. {novel.chinese_title}({novel.title}) 翻译进度{novel.translated_chapter}/{novel.total_chapter}\n\n'
-    release_text += '## **Translated by Summerkirakira**'
-    release_text += f'TAG={time.time()}'
+    release_text += f'## **Translated by Summerkirakira**'
     sys.stdout = sys.__stdout__
     print(release_text)
+    for file in Path(__file__).parent.glob('*'):
+        print(file.name)
